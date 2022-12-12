85d09830ff7532f3bb0bc84f7adedc86a459b470
Tanapot Chotiwatsakun <61749404+Touchalert@users.noreply.github.com>
2022-12-12 12:44:08 +0700

    update

--- src/pages/welcome/Login.jsx
+++ src/pages/welcome/Login.jsx
@@ -1,15 +1,29 @@
 import "../../styles/login.css";
 import logo from '../../img/login.png'; 
 import { Link } from "react-router-dom";
-// import axios from "axios";
-// import enurl from "../../api/environment";
+import axios from "axios";
+import enurl from "../../api/environment";
+
 const Login = () => {
-  
+  const url = enurl.apiUrl;
+  async function auth() {
+    let temp = {
+      Username: document.getElementById('eMail').value,
+      Password: document.getElementById('psw').value
+    }
+    await axios.post(`${url}login`,temp).then(response => {
+      if(response.data === 0){
+        console.log(response)
+        sessionStorage.setItem();   
+      }else{
+        console.log(response)
+      }
+    }).catch(ex =>{
+      throw ex
+    })
+  }
   return (
     <>
-    {/* function login() {
-      axios.post(`${enurl}/auth`).then((response,req))
-    } */}
     <div className="container-login">
       <div className="wrap-container">
         <div className="login-pic" style={{willChange:'tranform'}&&{
@@ -23,12 +37,12 @@ const Login = () => {
             </span>
 
             <div className="wrap-input"> 
-              <label for="uname"><b>Username</b></label>
+              <label for="uname" id="E_mail"><b>E-Mail</b></label>
                 <input type="text" placeholder="Enter Username" name="uname"   />
-              <label for="psw"><b>Password</b></label> 
+              <label for="psw" id="psw"><b>Password</b></label> 
                 <input type="text" placeholder="Enter Password" name="uname"   /> 
               
-              <button type="submit">Login</button>
+              <button type="submit" onClick={auth}>Login</button>
               
               <label>
                 <input type="checkbox" checked="checked" name="remember"/> Remember me
